90c33b2cd8ada8b8f2fbdc8172e836231758182e
Kenfenheuer <Maximilian.Kenfenheuer@siegwerk.com>
2018-07-23 09:38:51 +0200

    Added text classification example

---
+++ TextClassification/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace TextClassification
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 31);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(671, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Training Progress";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(671, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(16, 86);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(667, 238);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 339);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ TextClassification/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Annytab.Stemmer;
+
+using NeuralNet.Training.GeneticEvolve;
+using NeuralNet.Network.Interfaces;
+using NeuralNet.Math;
+
+namespace TextClassification
+{
+    public partial class Form1 : Form
+    {
+        GeneticEvolver evolver;
+        int tests = 10;
+        List<double[]> testInputs = new List<double[]>();
+        List<double[]> testOutputs = new List<double[]>();
+        Dictionary<string, string[]> testCases = new Dictionary<string, string[]>();
+        List<string> words = new List<string>();
+
+        EnglishStemmer stemmer = new EnglishStemmer();
+
+        public Form1()
+        {
+            string str = File.ReadAllText("Samples.json");
+            testCases = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, string[]>>(str);
+
+
+            foreach (string key in testCases.Keys)
+            {
+                for (int i = 0; i < testCases[key].Length; i++)
+                {
+                    testCases[key][i] = String.Join(" ", stemmer.GetSteamWords(testCases[key][i].Split(' ')));
+
+                }
+            }
+            foreach (string key in testCases.Keys)
+            {
+                for (int i = 0; i < testCases[key].Length; i++)
+                {
+                    string[] strings = testCases[key][i].Split(' ');
+                    foreach (string stringa in strings)
+                    {
+                        if (!words.Contains(stringa))
+                            words.Add(stringa);
+                    }
+                }
+            }
+
+            foreach (string key in testCases.Keys)
+            {
+                for (int i = 0; i < testCases[key].Length; i++)
+                {
+                    double[] inputs = new double[words.Count];
+                    double[] outputs = new double[testCases.Keys.Count];
+                    string[] strings = testCases[key][i].Split(' ');
+                    foreach (string stringa in strings)
+                    {
+                        inputs[words.IndexOf(stringa)] = 1;
+                    }
+
+                    outputs[testCases.Keys.ToList().IndexOf(key)] = 1;
+                    testInputs.Add(inputs);
+                    testOutputs.Add(outputs);
+                }
+            }
+            evolver = new GeneticEvolver(layerRange: new Tuple<int, int>(Math.Min(testInputs[0].Length, testOutputs[0].Length), Math.Max(testInputs[0].Length, testOutputs[0].Length)),
+                                                            layerLength: new Tuple<int, int>(3, 5),
+                                                            evaluationFunc: this.Evaluate,
+                                                            inputs: testInputs[0].Length,
+                                                            outputs: testOutputs[0].Length,
+                                                            activationFunction: ActivationFunc,
+                                                            mutationFunction: MutationFunc);
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Task t = Learn();
+            t.GetAwaiter().OnCompleted(() =>
+            {
+                MessageBox.Show("Learning Finished!");
+            });
+        }
+
+        async Task Learn()
+        {
+            evolver.Init();
+            while (evolver.Generation < 500 && evolver.MaxFitness < 1)
+            {
+                await evolver.Evaluate();
+                if (evolver.Generation % 10 == 0)
+                {
+                    Console.WriteLine("Generation {0} finished with max fitness {1}", evolver.Generation, evolver.MaxFitness);
+                    Console.Title = String.Format("Generation {0}; Max fitness {1}", evolver.Generation, evolver.MaxFitness);
+                    Evaluate(evolver.BestNetwork);
+                }
+                evolver.Evolve();
+                this.progressBar1.Value = (int)(100 * evolver.MaxFitness);
+            }
+        }
+
+        double ActivationFunc(double input)
+        {
+            return Math.Pow(input, 3);
+        }
+
+        double Evaluate(INeuralNet network)
+        {
+            double[] fitnessTests = new double[testInputs.Count];
+            for (int i = 0; i < fitnessTests.Length; i++)
+            {
+                double[] inputs = testInputs[i];
+                double[] outs = network.Calculate(inputs);
+                double[] fit = new double[testOutputs[0].Length];
+                for (int i2 = 0; i2 < fit.Length; i2++)
+                {
+                    fit[i2] = Fitness(testOutputs[i][i2], outs[i2]);
+                }
+                fitnessTests[i] = fit.Average();
+            }
+            return fitnessTests.Average();
+        }
+
+        double Fitness(double expected, double real)
+        {
+            return 1 - Math.Abs(real - expected);
+        }
+
+        double[] MutationFunc(double maxFitness)
+        {
+            return new double[] { 0.5, 1 / 200 * maxFitness + 1 };
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            double[] inputs = new double[words.Count];
+            double[] outputs = new double[testCases.Keys.Count];
+            string[] strings = stemmer.GetSteamWords(textBox1.Text.Split(' '));
+            foreach (string stringa in strings)
+            {
+                if (words.Contains(stringa))
+                    inputs[words.IndexOf(stringa)] = 1;
+            }
+
+            string[] output = new string[outputs.Length];
+            outputs = evolver.BestNetwork.Calculate(inputs);
+            for (int i = 0; i < output.Length; i++)
+            {
+                output[i] = testCases.Keys.ToArray()[i] + " - " + outputs[i];
+            }
+
+            listBox1.Items.Clear();
+            listBox1.Items.AddRange(output);
+        }
+    }
+}
